f270c0f25fac552ef3ca4998bff1665c40e598ec
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2023-11-26 16:20:26 +0000

    Deploying to gh-pages from @ ShoTuPalush/goit-react-hw-05-movies@e5ba275d54358ef7794388e3624e2cad5614e9e4 🚀

---
+++ static/js/736.6907ae1c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[736],{736:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(87),c=n(184);function o(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("b",{children:["pages not found, back ",(0,c.jsx)(r.rU,{to:"/",children:"link"})," to Home"]})})}}}]);
+//# sourceMappingURL=736.6907ae1c.chunk.js.map
